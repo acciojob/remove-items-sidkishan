8b19189cb336d16f8ff224398cac005bea37c8e5
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-05-28 16:43:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function removeColor(){
+	let col = document.getElementById("colorSelect").value;
+	let arr = document.getElementById("colorSelect").children;
+	for(let opt of arr){
+		if(opt.innerText == col){
+			opt.remove();
+		}
+	}
+}
